88fed7b40e9c66600180cc9a07c056d622fdf437
Gabriel Hirakawa <gabriel.souza30@fatec.sp.gov>
2019-09-10 21:48:47 -0300

    bugfix animation about

--- src/pages/About/styleSidebar.js
+++ src/pages/About/styleSidebar.js
@@ -6,20 +6,30 @@ display: flex;
   justify-content: space-between;
   height: 100vh;
   background-color: #336699;
+
   -webkit-animation-name: animacao;
-  -webkit-animation-duration: 1.5s;
+  -moz-animation-name: animacao;
+  -o-animation-name: animacao;
+  animation-name: animacao;
+
+  -webkit-animation-duration: 0.5s;
+  -moz-animation-duration: 0.5s;
+  -o-animation-duration: 0.5s;
+  animation-duration: 0.5s;
+
+
   -webkit-animation-fill-mode: forwards;
+  -moz-animation-fill-mode: forwards;
+  -o-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
   
 
-  @keyframes animacao {
-   from {
-      width: 33%; 
-   }
-   to { 
-     width: 66%;
-   }
-}
+  @-webkit-keyframes animacao{ from {width: 33%} to {width: 66%} }
+  @-moz-keyframes animacao{ from {width: 33%} to {width: 66%} }
+  @-o-keyframes animacao{ from {width: 33%} to {width: 66%} }
+  @keyframes animacao{ from {width: 33%} to {width: 66%} }
 
+ 
 
 `;
 
